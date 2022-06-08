42f9ff7515547ac1b61d1b0dd1eacc76ab5eb155
maximfd <m.fedenyov@gmail.com>
2022-06-08 16:49:35 +0300

    Add switching projects functionality

--- src/modules/Events.js
+++ src/modules/Events.js
@@ -7,5 +7,27 @@ export default class Events {
 
         this._view.displayProjects(this._model.projectsList)
         this._view.displayTasks(this._model.getProject(this.currentProjectId))
+
+        this._view.projectsList.addEventListener('click', this.projectEvents.bind(this))
+
+        this.setActiveProject()
+    }
+
+    projectEvents(e) {
+        // Remove highlighting from current project 
+        const prev = document.querySelector(`[data-id="${this.currentProjectId}"]`)
+        if (prev) prev.classList.toggle('project_active')
+        // Set clicked project as current & highlight it 
+        const project = e.target.closest('li')
+        this.currentProjectId = project.dataset.id
+        this.setActiveProject()
+
+        // Display current project tasks list
+        this._view.displayTasks(this._model.getProject(this.currentProjectId))
+    }
+
+    setActiveProject() {
+        const current = document.querySelector(`[data-id="${this.currentProjectId}"]`)
+        if (current) current.classList.add('project_active')
     }
 }
